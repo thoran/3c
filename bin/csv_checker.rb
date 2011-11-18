557a8d757a7b81981866c8655916ec37d60caa87
thoran <mail@thoran.com>
2011-11-18 16:25:33 +1100

    0.6.0

--- bin/csv_checker.rb
+++ bin/csv_checker.rb
@@ -2,12 +2,12 @@
 # csv_checker.rb
 
 # 20111117, 18
-# 0.5.0
+# 0.6.0
 
-# Changes since 0.4: 
-# 1. Removed all of the core extensions used here or in config/checks.rb.  
-# 2. Moved @warnings and @errors from parse() into check(), where they should have been, and made them locals, thereby removing two effectively global instance variables.  
-# 3. Made use of a new method on SimpleCSV's eigenclass: collect(), which shortens the required amount of code quite a bit.  
+# Changes since 0.5: 
+# 1. check() has one less test prior to inserting a message or warning.  
+# 2. + filterer() (I had to think of a name!), which returns which filter to apply to filter.  
+# 3. ~ filter(), so as it makes use of filterer().  
 
 require '../lib/SimpleCSV.rbd/SimpleCSV'
 require '../lib/Switches'
@@ -32,10 +32,12 @@ end
 def check(parsed_row)
   warnings, errors = [], []
   checks.each do |check|
-    if check[:warning] && !check[:test].call(parsed_row)
-      warnings << check[:warning].call(parsed_row)
-    elsif check[:error] && !check[:test].call(parsed_row)
-      errors << check[:error].call(parsed_row)
+    if !check[:test].call(parsed_row)
+      if check[:warning]
+        warnings << check[:warning].call(parsed_row)
+      elsif check[:error]
+        errors << check[:error].call(parsed_row)
+      end
     end
   end
   {:warnings => warnings, :errors => errors}
@@ -45,17 +47,21 @@ def parse(data_file)
   CSVFile.collect(data_file, :headers => true, :row_separator => "\r\r\n"){|row| row.merge(check(row))}
 end
 
-def filter(parsed_data)
-  case switches.filter_type
-  when 'clean'; parsed_data.select{|e| e[:warnings].empty? && e[:errors].empty?}
-  when 'unclean'; parsed_data.select{|e| !e[:warnings].empty? || !e[:errors].empty?}
-  when 'warnings'; parsed_data.select{|e| !e[:warnings].empty?}
-  when 'no_warnings'; parsed_data.select{|e| e[:warnings].empty?}
-  when 'errors'; parsed_data.select{|e| !e[:errors].empty?}
-  when 'no_errors'; parsed_data.select{|e| e[:errors].empty?}
-  when 'everything'; parsed_data
+def filterer
+  @filterer ||= case switches.filter_type
+  when 'clean'; Proc.new{|e| e[:warnings].empty? && e[:errors].empty?}
+  when 'unclean'; Proc.new{|e| !e[:warnings].empty? || !e[:errors].empty?}
+  when 'warnings'; Proc.new{|e| !e[:warnings].empty?}
+  when 'no_warnings'; Proc.new{|e| e[:warnings].empty?}
+  when 'errors'; Proc.new{|e| !e[:errors].empty?}
+  when 'no_errors'; Proc.new{|e| e[:errors].empty?}
+  when 'everything'; Proc.new{|e| true}
   else; raise 'filter unrecognized'
   end
+end
+
+def filter(parsed_data)
+  parsed_data.select{|e| filterer.call(e)}
 end
 
 def display(filtered_data)
